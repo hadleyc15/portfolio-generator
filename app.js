c3f03bc8a7c68ef766f683c3d4d4bb8f34491075
Chris Hadley <hadleyc15@yahoo.com>
2020-06-25 13:35:31 -0600

    added npm inquirer to the project

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
-
+// const pageHTML = generatePage(portfolioData);
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
