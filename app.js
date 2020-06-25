21a795fc8062aac5e92706c892e0ba60e4de115f
Chris Hadley <hadleyc15@yahoo.com>
2020-06-25 14:53:10 -0600

    added html generation for projects

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const inquirer = require('inquirer');
-
-// const pageHTML = generatePage(portfolioData);
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+//const profileDataArgs = process.argv.slice(2);
+//const [name, github] = profileDataArgs;
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -58,9 +49,9 @@ const promptUser = () => {
 
 const promptProject = portfolioData => {
   // If there's no 'projects' array property, create one
-if (!portfolioData.projects) {
-  portfolioData.projects = [];
-}
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
   console.log(`
 =================
 Add a New Project
@@ -127,16 +118,23 @@ Add a New Project
   ]).then(projectData => {
     portfolioData.projects.push(projectData);
     if (projectData.confirmAddProject) {
-        return promptProject(portfolioData);
+      return promptProject(portfolioData);
     } else {
-        return portfolioData;
+      return portfolioData;
     }
-});
-  
+  });
+
 };
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+     });
+    
   });
